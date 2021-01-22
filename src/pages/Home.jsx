819e179a272b4a8b7638d442e74da817700165a1
Andrei G <andrei.grideanu@gmail.com>
2021-01-23 00:15:22 +0200

    JSON setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
             <Layout>
                  <Link to='/login'>Login</Link>
                 <h1>Home</h1> 
+                <button type='button' className='btn btn-primary'>Primary</button>
            </Layout>
         </div>
     )
