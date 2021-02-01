dbe0094b7b007319eacae4717e05464a29960754
Andrei G <andrei.grideanu@gmail.com>
2021-02-01 16:27:33 +0200

    Firebase initialize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './config/firebase.js';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Careers from './pages/Careers.jsx';
@@ -8,22 +13,47 @@ import {Route, Switch} from 'react-router-dom';
 import Category from './pages/Category';
 import Terms from './/pages/TermsAndConditions';
 
-function App() {
-  return (
-    <div className="App bg-secondary">
-      <Switch>
-        <Route path='/' component={Home} exact/>
-        <Route path='/login' component={Login}/>
-        <Route path='/about' component={About}/>
-        <Route path='/careers' component={Careers}/>
-        <Route path='/termsandconditions' component={Terms}/>
-        <Route path='/category/:categoryName' component={Category}/>
-        <Route path='*' component={Page404}/>
-      </Switch>
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    console.log(this.props);
+
+    return (
+     <div className="App bg-secondary">
+        <Switch>
+          <Route path='/' component={Home} exact/>
+          <Route path='/login' component={Login}/>
+          <Route path='/about' component={About}/>
+          <Route path='/careers' component={Careers}/>
+          <Route path='/termsandconditions' component={Terms}/>
+          <Route path='/category/:categoryName' component={Category}/>
+          <Route path='*' component={Page404}/>
+        </Switch>
 
 
-    </div>
+      </div>
   );
+  }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwrV8cZ3eG557mMDKf0USv4vsdqLMm7vE",
+    authDomain: "top-foods-cef81.firebaseapp.com",
+    projectId: "top-foods-cef81",
+    storageBucket: "top-foods-cef81.appspot.com",
+    messagingSenderId: "883176583870",
+    appId: "1:883176583870:web:013ece55593b977289a4ef"
+  };
+
+  export default firebaseConfig;
